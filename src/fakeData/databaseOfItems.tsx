1e5a98d3b11e0cce521706823c7c60effd11a507
Foncemere <karltag2018@gmail.com>
2023-02-23 19:11:59 +0800

    Updated data structure

--- src/fakeData/databaseOfItems.tsx
+++ src/fakeData/databaseOfItems.tsx
@@ -1,44 +1,53 @@
 export const DatabaseOfItems = {
   toys: {
-    duck: {
-      price: 12,
-      description: "It's an ordinary duck.",
-    },
-    ball: {
-      price: 13,
-      description: "It's a ball.",
-    },
-    doll: {
-      price: 15,
-      description: "It's a doll.",
+    title: 'Toys',
+    items: {
+      duck: {
+        price: 12,
+        description: "It's an ordinary duck.",
+      },
+      ball: {
+        price: 13,
+        description: "It's a ball.",
+      },
+      doll: {
+        price: 15,
+        description: "It's a doll.",
+      },
     },
   },
   jewelry: {
-    rock: {
-      price: 500,
-      description: "It's an ordinary rock.",
-    },
-    gemstone: {
-      price: 700,
-      description: "It's a nice gem.",
-    },
-    diamond: {
-      price: 1000,
-      description: "It's a diamond.",
+    title: 'Jewelries',
+    data: {
+      rock: {
+        price: 500,
+        description: "It's an ordinary rock.",
+      },
+      gemstone: {
+        price: 700,
+        description: "It's a nice gem.",
+      },
+      diamond: {
+        price: 1000,
+        description: "It's a diamond.",
+      },
     },
   },
   clothes: {
-    shirt: {
-      price: 150,
-      description: "It's a shirt.",
-    },
-    pants: {
-      price: 200,
-      description: "It's a pair of pants.",
-    },
-    hat: {
-      price: 100,
-      description: "It's a hat.",
+    title: 'Clothes',
+    data: {
+      shirt: {
+        price: 150,
+        description: "It's a shirt.",
+      },
+      pants: {
+        price: 200,
+        description: "It's a pair of pants.",
+      },
+      hat: {
+        price: 100,
+        description: "It's a hat.",
+      },
     },
   },
 };
