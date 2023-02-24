f6e99576c8b285d22eede382abf460821bdb4ad7
Foncemere <karltag2018@gmail.com>
2023-02-24 10:19:13 +0800

    Tested usage of zustand's store

--- src/screens/AisleScreen/index.tsx
+++ src/screens/AisleScreen/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, FlatList, Text, View} from 'react-native';
 import zustardStore from '../../store';
 import BaseStyles from '../../baseStyling/styles';
 
 const AisleScreen = props => {
-  const store = zustardStore();
-  console.log(props.route.params.id);
+  const aisle = zustardStore().wholestore[props.route.params.id];
+
+  const renderAisleItems = ({item, index}) => {
+    return (
+      <View style={{borderWidth: 1, marginVertical: 16, padding: 12}}>
+        <Text>{item}</Text>
+        <Text>{aisle.items[item].description}</Text>
+        <Text>${aisle.items[item].price}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={BaseStyles.standard_border}>
       <View style={BaseStyles.header}>
         <Button title={'Go back'} onPress={() => props.navigation.goBack()} />
-        <Text>{store.wholestore[props.route.params.id]?.title}</Text>
+        <Text>{aisle?.title}</Text>
       </View>
+      <FlatList data={Object.keys(aisle.items)} renderItem={renderAisleItems} />
     </View>
   );
 };
