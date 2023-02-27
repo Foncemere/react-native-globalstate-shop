b8da5bca51f88b9f18b27c70d63c173ad89a1302
Foncemere <karltag2018@gmail.com>
2023-02-27 19:06:10 +0800

    added name

--- src/fakeData/databaseOfItems.tsx
+++ src/fakeData/databaseOfItems.tsx
@@ -3,14 +3,17 @@ export const DatabaseOfItems = {
     title: 'Toys',
     data: {
       duck: {
+        name: 'Rubber Ducky',
         price: 12,
         description: "It's an ordinary duck.",
       },
       ball: {
+        name: 'Big Ball',
         price: 13,
         description: "It's a ball.",
       },
       doll: {
+        name: 'Lady Mary',
         price: 15,
         description: "It's a doll.",
       },
@@ -20,14 +23,17 @@ export const DatabaseOfItems = {
     title: 'Jewelries',
     data: {
       rock: {
+        name: 'Martian Rock',
         price: 500,
         description: "It's an ordinary rock.",
       },
       gemstone: {
+        name: 'Red Ruby',
         price: 700,
         description: "It's a nice gem.",
       },
       diamond: {
+        name: 'Pristine Diamond',
         price: 1000,
         description: "It's a diamond.",
       },
@@ -37,14 +43,17 @@ export const DatabaseOfItems = {
     title: 'Clothes',
     data: {
       shirt: {
+        name: 'Shirt',
         price: 150,
         description: "It's a shirt.",
       },
       pants: {
+        name: 'Pants',
         price: 200,
         description: "It's a pair of pants.",
       },
       hat: {
+        name: 'Top hat',
         price: 100,
         description: "It's a hat.",
       },
@@ -54,14 +63,17 @@ export const DatabaseOfItems = {
     title: 'Books',
     data: {
       romance: {
+        name: "Neighbor's wife",
         price: 150,
         description: "It's a romance novel.",
       },
       suspense: {
+        name: 'This Is Almost Scary, But a Different Genre',
         price: 200,
         description: "It's a suspense novel.",
       },
       selfHelp: {
+        name: 'Your Mini Therapist',
         price: 100,
         description: "It's a self-help book.",
       },
