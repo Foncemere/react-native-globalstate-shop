9a280e744e095fb7a86f89d2c3fad18bf36b9c4a
Foncemere <karltag2018@gmail.com>
2023-02-27 19:01:32 +0800

    Made fix for addToCart

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,7 +4,10 @@ import DatabaseOfItems from '../fakeData/databaseOfItems';
 const zustardStore = create(set => ({
   wholestore: DatabaseOfItems,
   cart: [],
-  addToCart: item => set(state => ({cart: state.cart.push(item)})),
+  addToCart: item =>
+    set(state => {
+      return {cart: state.cart.concat(item)};
+    }),
 }));
 
 export default zustardStore;
