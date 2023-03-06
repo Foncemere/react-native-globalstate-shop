e3f260f6e04f8c89bd1d3728a6e544a40c0ba365
Foncemere <karltaguibao@gmail.com>
2023-03-06 18:16:50 +0800

    Updated to fix accessing data

--- src/screens/AisleScreen/index.tsx
+++ src/screens/AisleScreen/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {Button, FlatList, Text, TouchableOpacity, View} from 'react-native';
 import zustardStore from '../../store';
 import BaseStyles from '../../baseStyling/styles';
+import {CategoryTitle} from '../../fakeData/databaseOfItems';
 
 const AisleScreen = props => {
-  const aisle = zustardStore.getState().wholestore[props.route.params.id];
-  const add = zustardStore.getState().addToCart;
+  const wholestore = zustardStore.getState().wholestore;
+  const aisle = Object.values(wholestore).filter(item => {
+    return item?.category === props.route.params.id;
+  });
 
   const renderAisleItems = ({item, index}) => {
     return (
@@ -13,24 +16,21 @@ const AisleScreen = props => {
         style={{borderWidth: 1, marginVertical: 16, padding: 12}}
         onPress={() => {
           props.navigation.navigate('ItemDetails', {
-            id: item,
-            category: props.route.params.id,
+            id: item.id,
           });
         }}>
-        <Text>{item}</Text>
-        <Text>{aisle.data[item].description}</Text>
-        <Text>${aisle.data[item].price}</Text>
+        <Text>{item.name}</Text>
+        <Text>${item.price}</Text>
       </TouchableOpacity>
     );
   };
-
   return (
     <View style={BaseStyles.standard_border}>
       <View style={BaseStyles.header}>
         <Button title={'Go back'} onPress={() => props.navigation.goBack()} />
-        <Text>{aisle?.title}</Text>
+        <Text>{CategoryTitle[props.route.params.id]}</Text>
       </View>
-      <FlatList data={Object.keys(aisle.data)} renderItem={renderAisleItems} />
+      <FlatList data={aisle} renderItem={renderAisleItems} />
     </View>
   );
 };
